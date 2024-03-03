0626bd4d76a6ad18aa767fba78a1fe4beb540f22
nasirsabir <muhammednsrsbr@gmail.com>
2024-03-03 13:33:34 +0300

    I have updated the app.
    Now it is able to
    +Show a UIprogressView to demonstrate the progress of countdown.
    +Play an alarm sound after the countdown finished
    +The Text label updates itself with your commands

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,58 +7,86 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var countdownLabel: UILabel!
-
-    let hardnessTime = ["Soft": (5*60),"Medium": 7*60,"Hard": 12*60]
-    var timer: Timer?
-    var countdownSeconds = 0
+    var player: AVAudioPlayer!
+    override func viewDidLoad() {super.viewDidLoad()}
     
-    @IBAction func hardenssSelected(_ sender: UIButton) {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var progressLabel: UIProgressView!
+    
+    let hardnessTime = ["Soft": (5),"Medium": 7,"Hard": 12]
+    var timer: Timer?
+    var TotalSeconds = 0
+    var SecondsPassed = 0
+    
+    @IBAction func hardenssSelected(_ sender: UIButton) 
+    {
         let hardness = sender.currentTitle!
         var time = 0
-        switch hardness {
-        case "Soft":
-            time = hardnessTime["Soft"]!
-            startCountdown(time)
-            
-        case "Medium":
-            time = hardnessTime["Medium"]!
-            startCountdown(time)
-            
-        case "Hard":
-            time = hardnessTime["Hard"]!
-            startCountdown(time)
-            
-        default:
-            print("Error")
+        SecondsPassed = 0
+        progressLabel.progress = 0.0
+        titleLabel.text = hardness
+        
+        switch hardness 
+        {
+            case "Soft":
+                time = hardnessTime["Soft"]!
+                startCountdown(time)
+                
+            case "Medium":
+                time = hardnessTime["Medium"]!
+                startCountdown(time)
+                
+            case "Hard":
+                time = hardnessTime["Hard"]!
+                startCountdown(time)
+                
+            default:
+                print("Error")
         }
     }
-        func startCountdown(_ seconds: Int) {
-               // Invalidate previous timer if exists
-               timer?.invalidate()
-               
-               // Initialize the countdownSeconds
-               countdownSeconds = seconds
-               
-               // Update the label with initial countdown value
-               countdownLabel.text = "\(countdownSeconds)"
-               
-               // Initialize and start a new timer
-               timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCountdown), userInfo: nil, repeats: true)
-           }
+    
+    func startCountdown(_ seconds: Int)
+    {
+        // Invalidate previous timer if exists
+        timer?.invalidate()
+        
+        // Initialize the countdownSeconds
+        TotalSeconds = seconds
+        
+        // Initialize and start a new timer
+        timer = Timer.scheduledTimer(timeInterval: 1, target:self, selector: #selector(updateCountdown), userInfo: nil, repeats: true)
+    }
            
-           @objc func updateCountdown() {
-               if countdownSeconds > 0 {
-                   countdownSeconds -= 1
-                   countdownLabel.text = "\(countdownSeconds)"
-               } else {
-                   // Countdown has finished, invalidate timer
-                   timer?.invalidate()
-                   // You can perform any action here when the countdown finishes
-                   countdownLabel.text = "Countdown Finished!"
-               }
-           }
+    @objc func updateCountdown() 
+    {
+        if SecondsPassed < TotalSeconds
+        {
+            SecondsPassed += 1
+            progressLabel.progress += Float(1) / Float(TotalSeconds)
+            print(TotalSeconds, SecondsPassed)
+        }
+        else
+        {
+            // Countdown has finished, invalidate timer
+            timer?.invalidate()
+            // You can perform any action here when the countdown finishes
+            titleLabel.text = "Countdown Finished!"
+            playSound(soundName: "alarm_sound")
+        }
+    }
+    
+    func playSound(soundName: String)
+    {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
 }
